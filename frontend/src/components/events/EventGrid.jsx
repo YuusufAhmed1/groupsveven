c07add6ac591184d290149dd5d0c3c85018dc68c
sahracabdullahi <sahracabdullahi00@gmail.com>
2026-08-24 22:30:58 -0700

    update end qr code

--- frontend/src/components/events/EventGrid.jsx
+++
@@ -1,6 +0,0 @@
-import EventCard from './EventCard.jsx'
-
-function EventGrid({ events }) {
-  return <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">{events.map((event) => <EventCard key={event._id} event={event} />)}</div>
-}
-export default EventGrid
--- frontend/src/components/layout/MainLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer.jsx'
-import Header from './Header.jsx'
-
-function MainLayout() {
-  return <div className="flex min-h-screen flex-col"><Header /><div className="flex-1"><Outlet /></div><Footer /></div>
-}
-
-export default MainLayout
