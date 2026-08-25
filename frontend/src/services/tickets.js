b674f4c08f47b97931d8236f509a61dac81ce82a
sahracabdullahi <sahracabdullahi00@gmail.com>
2026-08-24 23:23:59 -0700

    ticktes.js

--- frontend/src/services/tickets.js
+++ frontend/src/services/tickets.js
@@ -6,5 +6,6 @@ export const verifyTicket = (qrToken) => api.post('/tickets/verify', { qrToken }
 export const getOrganizerStats = () => api.get('/organizer/stats').then((response) => response.data)
 export const getAdminUsers = () => api.get('/admin/users').then((response) => response.data)
 export const getAdminEvents = () => api.get('/admin/events').then((response) => response.data)
-export const getAdminTickets = () => api.get('/admin/tickets').then((response) => response.data)
 export const getAdminStats = () => api.get('/admin/stats').then((response) => response.data)
+export const getAdminTickets = () => api.get('/admin/tickets').then((response) => response.data)
+
